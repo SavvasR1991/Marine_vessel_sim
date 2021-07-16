49843482c48df092e36d8a488837cdb045ec952c
SavvasR1991 <41859521+SavvasR1991@users.noreply.github.com>
2021-07-16 10:06:50 +0300

    Add files via upload

---
+++ Movement_Plotting-TestScripts/Movement_Plotting_Graphics/Source/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+
+from graphics_Creator import *
+
+
+def main():
+    simulation_graphics_animation([0,0,0] , 8, 2, 8, 10)
+      
+
+if __name__ == "__main__":
+    main()
